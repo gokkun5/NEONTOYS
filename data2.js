72e78b3ed67bebfbde849c3a9c474a6cec16fbca
gokkun5 <suikago5@gmail.com>
2026-04-27 20:25:35 +0900

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,252 @@
+window.figures2 = [
+ 
+
+
+
+
+
+
+
+{
+series:"vtu",
+name:"赤見かるびの「お笑いかるび塾」ちょこのせプレミアムフィギュア“赤見かるび”",
+search:"あかみかるび",
+type:"tyoko",
+price:400,
+date: "",
+img:"images/vtu/karubi_tyoko2.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"witti",
+name:"TVアニメ『ウィッチウォッチ』Yumemirize“若月ニコ”",
+search:"わかつきにこ",
+type:"yume",
+price:400,
+date: "",
+img:"images/witti/niko_yume.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"nikke",
+name:"勝利の女神：NIKKEYumemirize“N102”",
+search:"えぬ102",
+type:"yume",
+price:350,
+date: "",
+img:"images/nikke/n102_yume.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"buruaka",
+name:"ブルーアーカイブYumemirize“ホシノ”",
+search:"ほしの",
+type:"yume",
+price:500,
+date: "",
+img:"images/buruaka/hosino_yume.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"gotoyome",
+name:"五等分の花嫁∬　Desktop Cute フィギュア　中野四葉～描き下ろしCat room wear ver.～",
+search:"なかのよつば",
+type:"desk",
+price:600,
+date: "",
+img:"images/gotoyome/yotuba_desk_neko.jpg",
+source:"https://www.taito.co.jp/"
+},
+
+{
+series:"wanpi",
+name:"ワンピース GLITTER&GLAMOURS-NAMI-SPECIAL COLOR ver.",
+search:"なみ　ナミ",
+type:"gli",
+price:600,
+date: "",
+img:"images/wanpi/nami_gli_sp.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"bururo",
+name:"TVアニメ『ブルーロック』 凪 誠士郎 フィギュア-“青い監獄”11傑-",
+search:"なぎせいしろう",
+type:"",
+price:1200,
+date: "",
+img:"images/bururo/nagi_11.jpg",
+source:"https://bsp-prize.jp/"
+},    
+
+ {
+series:"wanpi",
+name:"ワンピース アニメロゴライト",
+search:"",
+type:"",
+price:800,
+date: "2026-04-20",
+img:"images/wanpi/rogo.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"mar",
+name:"MARVELACT/CUTプレミアムフィギュア“シルバーサーファー”",
+search:"しるばーさーふぁー",
+type: "act",
+price:800,
+date: "",
+img:"images/mar/siruba.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"mazyotabi",
+name:"魔女の旅々　ふぃぐきゅーぶ イレイナ",
+search:"いれいな",
+type:"figc",
+price:800,
+date: "2026-04-20",
+img:"images/mazyotabi/figkyu.jpg",
+source:"https://p.eagate.573.jp/game/prize/collection/top/index.html"
+},
+
+{
+series:"oba",
+name:"BiCute Bunnies Figure ーアルベド・another costumeー",
+search:"あるべど",
+type:"bani",
+price:900,
+date: "2026-04-20",
+img:"images/oba/aru_bani_ano.jpg",
+source:"https://charahiroba.com/"
+},
+
+{
+series:"naruto",
+name:"NARUTO-ナルト- 疾風伝 Noir Edge Collection-UZUMAKI NARUTO-",
+search:"なると　ナルト",
+type:"noir",
+price:1600,
+date: "2026-04-20",
+img:"images/naruto/naruto_noir.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"furi-ren",
+name:"葬送のフリーレン おおきなSOFVIMATES～縦ロールになっちゃった～",
+search:"ふりーれん",
+type: "",
+price:900,
+date: "2026-04-20",
+img:"images/furi-ren/furi_sof_tate.jpg",
+source:"https://bsp-prize.jp/"
+},    
+
+{
+series:"gundam",
+name:"『機動戦士ガンダムSEED FREEDOM』GLITTER&GLAMOURS-Lunamaria Hawke- Racing ver.",
+search:"ルナマリア　るなまりあ",
+type:"",
+price:600,
+date: "2026-04-20",
+img:"images/gundam/runa_gli_re.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"nikke",
+name:"公女殿下の家庭教師Yumemirize“ティナ・ハワード”",
+search:"てぃなはわーど",
+type:"yume",
+price:400,
+date: "",
+img:"images/kozyo/texina.jpg",
+source:"https://segaplaza.jp/"
+},
+
+{
+series:"zyozyo",
+name:"ジョジョの奇妙な冒険 黄金の風 フィグライフ！ Mt",
+search:"",
+type:"figl",
+price:800,
+date: "2026-04-20",
+img:"images/zyozyo/mt_figl.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"maebasi",
+name:"前橋ウィッチーズDesktop×DecorateCollections“北原キョウカ”",
+search:"きたはらきょうか",
+type: "ddc",
+price:300,
+date: "",
+img:"images/maebasi/kyouka_ddc.jpg",
+source:"https://segaplaza.jp/"
+},
+
+ {
+series:"wanpi",
+name:"『ONE PIECE FILM RED』 DXF～THE GRANDLINE MEN～vol.4",
+search:"サンジ　さんじ",
+type:"dxf",
+price:400,
+date: "",
+img:"images/wanpi/sanzi_dxf4.jpg",
+source:"https://bsp-prize.jp/"
+},
+
+{
+series:"pro",
+name:"プロジェクトセカイカラフルステージ！feat.初音ミクDesktop×DecorateCollections“ストリートのセカイの鏡音レン”",
+search:"かがみねれん",
+type: "ddc",
+price:600,
+date: "",
+img:"images/pro/ren_ddc.jpg",
+source:"https://segaplaza.jp/"
+},
+
+  {
+series:"purikyua",
+name:"ふたりはプリキュア フィグライフ! カードコミューン型スタンド",
+search:"めっぷる",
+type:"figl",
+price:600,
+date: "",
+img:"images/purikyua/meppu.jpg",
+source:"https://bsp-prize.jp/"
+},
+ 
+ {
+series:"purikyua",
+name:"ふたりはプリキュア フィグライフ! カードコミューン型スタンド",
+search:"みっぷる",
+type:"figl",
+price:600,
+date: "",
+img:"images/purikyua/mippu.jpg",
+source:"https://bsp-prize.jp/"
+},
+ 
+    {
+    series:"ranma",
+    name:"TVアニメ「らんま1/2」ちょこのせプレミアムフィギュア“らんま”",
+    search:"",
+    type:"tyoko",
+    price:400,
+    date: "",
+    img:"images/ranma/ranma_tyoko.jpg",
+    source:"https://segaplaza.jp/"
+  }
+
+];
