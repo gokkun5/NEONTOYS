f8f2ccc899eb7dff63d5b90b761bf35229794445
gokkun5 <suikago5@gmail.com>
2026-06-01 06:45:36 +0900

    henko

--- data3.js
+++ data3.js
@@ -2,7 +2,10 @@ window.figures3 = [
 
 
 
-
+{series:"king", name:"キングダム オギコフィギュア", search:"おぎこ", type:"", price:500, date:"2026-05-27", img:"images/king/ogiko.jpg", source:"https://bsp-prize.jp/"},
+{series:"tomuzyeri", name:"トムとジェリー ビッグソフビフィギュア-Yummy Bread-", search:"じぇりー　たふぃー　ジェリー　タフィー", type:"", price:500, date:"2026-05-27", img:"images/tomuzyeri/zyeritafi.jpg", source:"https://bsp-prize.jp/"},
+{series:"sonota", name:"UNIVERSAL おおきなSOFVIMATES～おさるのジョージ～", search:"おさるのじょーじ", type:"sof", price:400, date:"2026-05-26", img:"images/sonota/zyozi_sof.jpg", source:"https://bsp-prize.jp/"},
+{series:"dandadan", name:"TVアニメ『ダンダダン』 フィグライフ!モモ", search:"もも", type:"figl", price:400, date:"2026-05-26", img:"images/dandadan/momo_figl.jpg", source:"https://bsp-prize.jp/"},
 {series:"miku", name:"初音ミクシリーズ　Luminasta　“初音ミク 16th Anniversary”KEI Ver.", search:"はつねみく", type:"lum", price:700, date:"1899-12-30", img:"images/miku/16_key.jpg", source:"https://segaplaza.jp/"},
 {series:"db", name:"ドラゴンボールZ MATCH MAKERS 魔人ブウ（VS超サイヤ人3孫悟空）", search:"まじんぶう", type:"mat", price:600, date:"1899-12-30", img:"images/db/bu_mat_goku.jpg", source:"https://bsp-prize.jp/"},
 {series:"king", name:"キングダム Grandistaｰ桓騎ｰ", search:"かんき", type:"gra", price:500, date:"2026-05-27", img:"images/king/kanki_gra.jpg", source:"https://bsp-prize.jp/"},
