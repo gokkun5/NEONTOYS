9ada9d5c6fe5a43e7162c6e5f17503f8dfee141e
gokkun5 <suikago5@gmail.com>
2026-05-31 17:02:35 +0900

    hennko

--- data3.js
+++ data3.js
@@ -1,4 +1,11 @@
 window.figures3 = [
 
 
+
+
+{series:"kuresin", name:"クレヨンしんちゃん コスプレしんちゃんフィギュアvol.3", search:"野原しんのすけ　のはらしんのすけ", type:"", price:600, date:"1899-12-30", img:"images/kuresin/kosu_buri.jpg", source:"https://bsp-prize.jp/"},
+{series:"kuresin", name:"クレヨンしんちゃん コスプレしんちゃんフィギュアvol.3", search:"野原しんのすけ　のはらしんのすけ", type:"", price:600, date:"1899-12-30", img:"images/kuresin/kosu_kan.jpg", source:"https://bsp-prize.jp/"},
+{series:"konan", name:"名探偵コナン XrossLink フィギュア“江戸川コナン”", search:"えどがわこなん", type:"xros", price:400, date:"1899-12-30", img:"images/konan/konan_xros.jpg", source:"https://segaplaza.jp/"},
+{series:"konan", name:"名探偵コナン XrossLink フィギュア“怪盗キッド”", search:"かいとうきっど", type:"xros", price:700, date:"1899-12-30", img:"images/konan/kiddo_xros.jpg", source:"https://segaplaza.jp/"},
+{series:"gorukamu", name:"TVアニメ『ゴールデンカムイ』XrossLinkフィギュア“白石由竹”", search:"しらいしよしたけ", type:"xros", price:300, date:"1899-12-30", img:"images/gorukamu/siraisi_xros.jpg", source:"https://segaplaza.jp/"},
 ];
