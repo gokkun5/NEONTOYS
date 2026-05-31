7e4d7f3dd124677185c61649e80ea4ae39514530
gokkun5 <suikago5@gmail.com>
2026-05-31 17:26:50 +0900

    henko

--- data3.js
+++ data3.js
@@ -3,6 +3,9 @@ window.figures3 = [
 
 
 
+{series:"miku", name:"初音ミクシリーズ　Luminasta　“初音ミク 16th Anniversary”KEI Ver.", search:"はつねみく", type:"lum", price:700, date:"1899-12-30", img:"images/miku/16_key.jpg", source:"https://segaplaza.jp/"},
+{series:"db", name:"ドラゴンボールZ MATCH MAKERS 魔人ブウ（VS超サイヤ人3孫悟空）", search:"まじんぶう", type:"mat", price:600, date:"1899-12-30", img:"images/db/bu_mat_goku.jpg", source:"https://bsp-prize.jp/"},
+{series:"king", name:"キングダム Grandistaｰ桓騎ｰ", search:"かんき", type:"gra", price:500, date:"2026-05-27", img:"images/king/kanki_gra.jpg", source:"https://bsp-prize.jp/"},
 {series:"kuresin", name:"クレヨンしんちゃん コスプレしんちゃんフィギュアvol.3", search:"野原しんのすけ　のはらしんのすけ", type:"", price:600, date:"1899-12-30", img:"images/kuresin/kosu_buri.jpg", source:"https://bsp-prize.jp/"},
 {series:"kuresin", name:"クレヨンしんちゃん コスプレしんちゃんフィギュアvol.3", search:"野原しんのすけ　のはらしんのすけ", type:"", price:600, date:"1899-12-30", img:"images/kuresin/kosu_kan.jpg", source:"https://bsp-prize.jp/"},
 {series:"konan", name:"名探偵コナン XrossLink フィギュア“江戸川コナン”", search:"えどがわこなん", type:"xros", price:400, date:"1899-12-30", img:"images/konan/konan_xros.jpg", source:"https://segaplaza.jp/"},
