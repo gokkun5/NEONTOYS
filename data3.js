319f663fd7b22b88ea504e224aaf78776a24ccde
gokkun5 <suikago5@gmail.com>
2026-06-01 07:52:51 +0900

    henko

--- data3.js
+++ data3.js
@@ -3,6 +3,17 @@ window.figures3 = [
 
 
 
+
+
+{series:"eva", name:"シン・エヴァンゲリオン劇場版Luminasta“真希波・マリ・イラストリアス”", search:"まきなみまりいらすとりあす", type:"lum", price:800, date:"1899-12-30", img:"images/eva/mari_lum.jpg", source:"https://segaplaza.jp/"},
+{series:"kimetu", name:"鬼滅の刃 ぬーどるストッパーフィギュア ―伊黒小芭内―", search:"いぐろおばない", type:"nusuto", price:4500, date:"1899-12-30", img:"images/kimetu/iguro_nusuto.jpg", source:"https://charahiroba.com/"},
+{series:"zyuzyutu", name:"呪術廻戦懐玉・玉折FIGURIZMα“五条悟”覚醒", search:"ごじょうさとる", type:"fig", price:1300, date:"1899-12-30", img:"images/zyuzyutu/gozyo_kakusei.jpg", source:"https://segaplaza.jp/"},
+{series:"kimetu", name:"鬼滅の刃　プレミアムちょこのせフィギュア“煉󠄁獄杏寿郎”", search:"れんごくきょうじゅろう", type:"tyoko", price:800, date:"1899-12-30", img:"images/kimetu/rengoku_tyoko_oni.jpg", source:"https://segaplaza.jp/"},
+{series:"mazyotabi", name:"魔女の旅々　AMP＋ イレイナ フィギュア～魔女服ver.～", search:"いれいな", type:"amp", price:1000, date:"1899-12-30", img:"images/mazyotabi/amp.jpg", source:"https://www.taito.co.jp/"},
+{series:"naruto", name:"NARUTO-ナルト- BANPRESTO FIGURE COLOSSEUM 造形忍界大戦 ロック・リー", search:"ろっくりー", type:"ninkai", price:2300, date:"1899-12-30", img:"images/naruto/ri_ninkai.jpg", source:"https://bsp-prize.jp/"},
+{series:"wanpi", name:"ワンピース THE出航-NAMI-", search:"なみ", type:"syukko", price:500, date:"1899-12-30", img:"images/wanpi/nami_syukko.jpg", source:"https://bsp-prize.jp/"},
+{series:"sanrio", name:"サンリオキャラクターズ コスプレフィギュアコレクション～マイメロディ・クロミ～", search:"まいめろでぃ", type:"", price:500, date:"2026-05-27", img:"images/sanrio/maimero_kosu.jpg", source:"https://bsp-prize.jp/"},
+{series:"sanrio", name:"サンリオキャラクターズ コスプレフィギュアコレクション～マイメロディ・クロミ～", search:"くろみ", type:"", price:500, date:"2026-05-27", img:"images/sanrio/kuromi_kosu.jpg", source:"https://bsp-prize.jp/"},
 {series:"uru", name:"ウルトラマンシリーズ 鎮座獣 コイン怪獣カネゴン", search:"こいんかいじゅうかねごん", type:"tinza", price:500, date:"2026-05-27", img:"images/uru/kane_tinza.jpg", source:"https://bsp-prize.jp/"},
 {series:"uru", name:"ウルトラマンシリーズ 鎮座獣 コイン怪獣カネゴン", search:"こいんかいじゅうかねごん", type:"tinza", price:500, date:"2026-05-27", img:"images/uru/kane_tinza_b.jpg", source:"https://bsp-prize.jp/"},
 {series:"monhan", name:"モンスターハンター 鎮座獣 アルシュベルド～夢から覚めて～", search:"あるしゅべると", type:"tinza", price:500, date:"2026-05-27", img:"images/monhan/arusyu_tinza.jpg", source:"https://bsp-prize.jp/"},
